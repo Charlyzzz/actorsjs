8c5df2aa6925d5835f25ea108bd544de2571257e
Erwin Debusschere <erwincdl@gmail.com>
2020-02-26 03:22:31 -0300

    Ask

--- actors.js
+++ actors.js
@@ -4,13 +4,24 @@ const generateId = () => crypto.randomBytes(16).toString('hex');
 
 const INIT_MSG = 'init';
 
+const BaseActor = {
+    spawned() {},
+    dispatch(action) {
+        const immediate = setImmediate(action);
+        let dispatchId = generateId();
+        this.dispatchs[dispatchId] = immediate;
+        return dispatchId;
+    }
+};
+
 const ActorWithInit = {
+    __proto__: BaseActor,
     spawned() {
-        setImmediate(() => this.context.tell(this.context.self, INIT_MSG));
+        this.dispatch(() => this.context.tell(this.context.self, INIT_MSG));
     },
     receive(msg, _sender) {
         if (msg === INIT_MSG) {
-            setImmediate(() => {
+            this.dispatch(() => {
                 this.context.initBehavior(this.context);
                 Object.setPrototypeOf(this, Actor);
             });
@@ -21,22 +32,23 @@ const ActorWithInit = {
 };
 
 const Actor = {
-    spawned() {},
+    __proto__: BaseActor,
     receive(msg, sender) {
         this.mailbox.push({ msg, sender });
-        setImmediate(() => {
-            this.checkMailbox();
+        this.dispatch(async () => {
+            await this.checkMailbox();
         });
     },
-    checkMailbox() {
+    async checkMailbox() {
         if (this.mailbox.length !== 0) {
             const msg = this.mailbox.shift();
-            this.processMessage(msg);
+            await this.processMessage(msg);
         }
     },
-    processMessage({ msg, sender }) {
+    async processMessage({ msg, sender }) {
+        this.busy = true;
         try {
-            this.context.behavior(msg, sender, this.context);
+            await this.context.behavior(msg, sender, this.context);
         } catch (e) {
             console.error(e);
         }
@@ -51,9 +63,10 @@ const Behaviors = {
 const actor = (initBehavior, behavior, name, system) => {
 
     const proto = initBehavior ? ActorWithInit : Actor;
-
     let actor = Object.create(proto);
+
     actor.mailbox = [];
+    actor.dispatchs = {};
 
     const logger = ['log', 'warn', 'error'].reduce((loggerSoFar, level) => {
         loggerSoFar[level] = (msg) => {
@@ -69,8 +82,14 @@ const actor = (initBehavior, behavior, name, system) => {
         tell(whom, msg, sender = name) {
             system.tell(whom, msg, sender);
         },
+        ask(whom, msg, timeout, sender = name) {
+            return this.system.ask(whom, msg, timeout, sender);
+        },
         scheduleOnce(whom, msg, delay, sender = name) {
             return system.scheduleOnce(whom, msg, delay, sender);
+        },
+        stop() {
+            this.system.stop(this.self);
         },
         cancelTimer: clearTimeout,
         logger,
@@ -92,35 +111,79 @@ const System = {
         return this.doSpawn(initBehavior, behavior, actorName);
     },
     doSpawn(initBehavior, behavior, name) {
-        if (this.lookup(name) !== this.deadLetterQueue)
+        if (this.lookup(name) !== this.lookup(this.deadLetters))
             throw new Error(`An actor with that name already exists. Name was [${name}]`);
         this.actors[name] = actor(initBehavior, behavior, name, this);
         return name;
     },
-    tell(whom, msg, sender = this.deadLetterQueue) {
+    tell(whom, msg, sender = this.deadLetters) {
         this.lookup(whom).receive(msg, sender);
     },
-    scheduleOnce(whom, msg, delay, sender = this.deadLetterQueue) {
+    scheduleOnce(whom, msg, delay, sender = this.deadLetters) {
         return setTimeout(() => this.tell(whom, msg, sender), delay);
     },
     lookup(actor) {
-        return this.actors[actor] || this.deadLetterQueue;
+        return this.actors[actor] || this.actors[this.deadLetters];
+    },
+    ask(whom, msg, timeout, sender = this.deadLetters) {
+        return new Promise((resolve, reject) => {
+            this.tell(this.replier, {
+                msg, whom, timeout, resolve, reject
+            }, sender);
+        });
+    },
+    stop(actor) {
+        Object.values(this.lookup(actor).dispatchs).forEach(clearImmediate);
+        delete this.actors[actor];
     }
 };
 
 const system = () => {
     const newSystem = Object.create(System);
-    newSystem.deadLetterQueue = newSystem.spawnSystemActor(deadLetterQueue, 'deadLetterQueue');
+    newSystem.deadLetters = newSystem.spawnSystemActor(deadLetters, 'deadLetters');
     newSystem.logger = newSystem.spawnSystemActor(logger, 'logger');
+    newSystem.replier = newSystem.spawnSystemActor(replier, 'replier');
     return newSystem;
 };
 
-const logger = ([level, msg, whom]) => {
-    console[level](`[${whom}]: ` + msg);
+const replies = {};
+
+
+const timeout = ({ interactionId, timeout }, sender, ctx) => {
+    const { reject, replyProxy } = replies[interactionId];
+    reject(new Error(`Timeout occured after ${timeout} ms`));
+    delete replies[interactionId];
+    ctx.system.stop(replyProxy);
 };
 
-const deadLetterQueue = (msg, ctx) => {
-    ctx.logger.log(`Received ${msg} from ${ctx.sender}`);
+const ask = ({ msg, whom, timeout, resolve, reject }, sender, ctx) => {
+    const interactionId = generateId();
+    const replyProxy = ctx.system.spawnSystemActor((reply, _sender, ctx) => {
+        const { resolve, timer } = replies[interactionId];
+        clearTimeout(timer);
+        resolve(reply);
+        delete replies[interactionId];
+        ctx.stop();
+    }, 'replyAdapter-' + generateId(), (ctx) => {
+        ctx.tell(whom, msg, ctx.self);
+    });
+    const timer = ctx.scheduleOnce(ctx.self, { type: 'timeout', interactionId, timeout }, timeout);
+    replies[interactionId] = {
+        resolve, timer, reject, replyProxy
+    };
+};
+
+const replier = ({ type, ...msg }, sender, ctx) => {
+    const action = type === 'timeout' ? timeout : ask;
+    action(msg, sender, ctx);
+};
+
+const logger = ([level, msg, whom]) => {
+    console[level](`[${whom}]: ` + JSON.stringify(msg));
+};
+
+const deadLetters = (msg, sender, ctx) => {
+    ctx.logger.log(`Received [${msg}] from ${sender}`);
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,19 +1,113 @@
 const { system, Behaviors } = require('./actors');
 
-const ping = (msg, sender, ctx) => {
-    ctx.logger.log(msg);
-    ctx.scheduleOnce(sender, 'ping', 1000);
+const timer = {
+    start() {
+        this.startMillis = this.getCurrentMillis();
+    },
+    track() {
+        return this.getCurrentMillis() - this.startMillis;
+    },
+    reset() {
+        this.start();
+    },
+    getCurrentMillis() {
+        return new Date().getTime();
+    }
 };
 
-const pong = (msg, sender, ctx) => {
-    ctx.logger.log(msg);
-    ctx.scheduleOnce(sender, 'pong', 1000);
+const pingPong = async (actorSystem) => {
+
+    const ping = (msg, sender, ctx) => {
+        ctx.logger.log(msg);
+        ctx.scheduleOnce(sender, 'ping', 1000);
+    };
+
+    const pong = (msg, sender, ctx) => {
+        ctx.logger.log(msg);
+        ctx.scheduleOnce(sender, 'pong', 1000);
+    };
+
+    const pongActor = actorSystem.spawnActor(pong, 'ponger');
+
+    actorSystem.spawnActor(ping, 'pinger', (ctx) => {
+        ctx.tell(pongActor, 'ping');
+    });
 };
 
-const mySystem = system();
-const pongActor = mySystem.spawnActor(pong, 'ponger');
+const ask = async (actorSystem) => {
 
-mySystem.spawnActor(ping, 'pinger', (ctx) => {
-    ctx.tell(pongActor, 'ping');
-});
+    const replier = actorSystem.spawnActor((msg, sender, ctx) => {
+        if (msg === 'rapido') {
+            ctx.tell(sender, 'Hola!');
+        } else if (msg === 'lento') {
+            ctx.scheduleOnce(sender, 'Hola! (mas lento)', 1000);
+        }
+    });
+    timer.start();
+    let respuesta = await actorSystem.ask(replier, 'rapido', 1000);
+    console.log({ respuesta, millis: timer.track() });
+    timer.reset();
+    respuesta = await actorSystem.ask(replier, 'lento', 2000);
+    console.log({ respuesta, millis: timer.track() });
+    try {
+        await actorSystem.ask(replier, 'lento', 1000);
+    } catch (e) {
+        console.log(e);
+    }
+};
 
+const askBetweenActors = async (actorSystem) => {
+
+    const dolar = actorSystem.spawnActor((msg, sender, ctx) => {
+        ctx.tell(sender, 83);
+    });
+
+    const catalogo = actorSystem.spawnActor((msg, sender, ctx) => {
+        const precios = {
+            'pc': 1500,
+            'tablet': 700
+        };
+        const precio = precios[msg];
+        precio && ctx.tell(sender, precio);
+    });
+
+    const mercadoLibre = actorSystem.spawnActor(async (msg, sender, ctx) => {
+        const precioEnDolares = await ctx.ask(catalogo, msg, 1000);
+        const dolarHoy = await ctx.ask(dolar, null, 1000);
+        ctx.tell(sender, dolarHoy * precioEnDolares);
+    });
+
+    let precioPc = await actorSystem.ask(mercadoLibre, 'pc', 1000);
+    console.log({ precioPc });
+};
+
+
+const scheduling = async (actorSystem) => {
+
+    const proveedorDeAes = actorSystem.spawnActor((msg, sender, ctx) => {
+        ctx.scheduleOnce(sender, 'a', 3000);
+    });
+
+    const abecedario = actorSystem.spawnActor(async (msg, sender, ctx) => {
+        switch (msg) {
+            case 'a':
+                const a = await ctx.ask(proveedorDeAes, null, 5000);
+                console.log(1);
+                ctx.tell(sender, a);
+                break;
+            case 'b':
+                ctx.tell(sender, 'b');
+                console.log(2);
+                break;
+        }
+    });
+
+    actorSystem.ask(abecedario, 'b', 1000);
+    actorSystem.ask(abecedario, 'a', 5000);
+    actorSystem.ask(abecedario, 'b', 1000);
+    actorSystem.ask(abecedario, 'b', 1000);
+
+};
+
+const actorSystem = system();
+scheduling(actorSystem);
